bfc77abee1a3de5f6da4af383044608ad3cc0b17
Sergey Galuzov <seregasuper2000@gmail.com>
2020-11-06 13:25:04 +0200

    first commit

---
+++ OLAP_Lab5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OLAP_Lab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
